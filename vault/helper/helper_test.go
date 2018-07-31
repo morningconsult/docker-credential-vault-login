b0902aa2872189a7ffeedcc0ef4f452de3eb2c12
Dilan Bellinghoven <ddbellinghoven@gmail.com>
2018-07-31 10:35:08 -0400

    vault/helper/helper_test.go: Attempts to read output of vault startup logs

--- vault/helper/helper_test.go
+++ vault/helper/helper_test.go
@@ -1,6 +1,7 @@
 package helper
 
 import (
+        "io/ioutil"
         "strconv"
         "testing"
         "gitlab.morningconsult.com/mci/docker-credential-vault-login/vault"
@@ -9,7 +10,13 @@ import (
 func TestTODO(t *testing.T) {
         port, err := strconv.Atoi(vault.VaultDevPort)
         if err != nil {
-                t.Fatalf("strconv.Atoi failed: %v", err)
+                t.Fatalf("strconv.Atoi failed: %v\n", err)
         }
         t.Logf("Test port: %d\n", port)
+
+        data, err := ioutil.ReadFile("testdata/vault_dev_server_output.txt")
+        if err != nil {
+                t.Fatalf("ReadFile failed: %v\n", err)
+        }
+        t.Logf("Vault Data:\n\n***\n\n%s\n***\n\n", string(data))
 }
