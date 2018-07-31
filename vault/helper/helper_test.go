f309bdcc3362600e39d90b9a29db16c14ca14a73
Dilan Bellinghoven <ddbellinghoven@gmail.com>
2018-07-31 13:24:28 -0400

    vault/helper/helper_test.go: Added test to check if client can write secret

--- vault/helper/helper_test.go
+++ vault/helper/helper_test.go
@@ -3,7 +3,7 @@ package helper
 import (
         "os"
         "testing"
-        
+
         vaultAPI "github.com/hashicorp/vault/api"
         "gitlab.morningconsult.com/mci/docker-credential-vault-login/vault"
 )
@@ -17,5 +17,11 @@ func TestTODO(t *testing.T) {
         if err != nil {
                 t.Fatalf("error creating new Vault API client: %v", err)
         }
-        
+        mySecret := map[string]interface{}{
+                "foo": "bar",
+        }
+        _, err = client.Logical().Write("secret/foo/bar", mySecret)
+        if err != nil {
+                t.Fatalf("error writing secret: %v", err)
+        }
 }
