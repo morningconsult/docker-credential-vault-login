2598c599cae94ac8d552f5ac0c997284ad1d920f
Dilan Bellinghoven <ddbellinghoven@gmail.com>
2018-07-31 17:56:55 -0400

    vault/testing.go: More helper methods

--- vault/helper/helper_test.go
+++ vault/helper/helper_test.go
@@ -1,47 +1,20 @@
 package helper
 
 import (
-        "bytes"
-        "encoding/json"
         "io/ioutil"
-        "net/http"
         "testing"
-        "time"
 
-        cleanhttp "github.com/hashicorp/go-cleanhttp"
         "gitlab.morningconsult.com/mci/docker-credential-vault-login/vault"
 )
 
 func TestTODO(t *testing.T) {
         var secret = "foo/bar"
-        client := cleanhttp.DefaultClient()
-        client.Timeout = time.Second * 60
 
-        info := vault.NewVaultTestServerInfo(t)
+        client := vault.NewTestClient(t)
 
-        data, err := json.Marshal(map[string]interface{}{
-                "data": map[string]string{
-                        "foo": "bar",
-                },
-        })
-        if err != nil {
-                t.Fatalf("error marshaling map: %v", err)
-        }
-        buf := bytes.NewBuffer(data)
-
-        req, err := http.NewRequest("PUT", info.Address + "/v1/secret/data/" + secret, buf)
-        req.Header.Set("X-Vault-Token", info.Token)
-        req.Header.Set("Content-Type", "application/json")
-        
-        resp, err := client.Do(req)
-        if err != nil {
-                t.Fatalf("error making HTTP request: %v", err)
-        }
+        resp := client.Write(t, secret, map[string]interface{}{"foo": "bar"})
         defer resp.Body.Close()
 
         body, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-                t.Fatalf("error reading response body: %v", err)
-        }
-        t.Logf("response body: %s\n", string(body))
+        t.Logf("Body: %s\n", string(body))
 }
--- vault/testing.go
+++ vault/testing.go
@@ -1,28 +1,92 @@
 package vault
 
 import (
+        "bytes"
+        "encoding/json"
+        "fmt"
+        "io/ioutil"
+        "net/http"
         "strconv"
+        "strings"
         "testing"
+
+        cleanhttp "github.com/hashicorp/go-cleanhttp"
 )
 
 var VaultDevPortString string
 
 var VaultDevRootToken string
 
-type VaultTestServerInfo struct {
-        Address string
-        Token   string
+type TestClient struct {
+        pathPrefix string
+        address    string
+        token      string
+        client     *http.Client
 }
 
-func NewVaultTestServerInfo(t *testing.T) *VaultTestServerInfo {
+func NewTestClient(t *testing.T) *TestClient {
         if _, err := strconv.Atoi(VaultDevPortString); err != nil {
                 t.Fatal("Vault listener port must be an integer")
         }
         if VaultDevRootToken == "" {
                 t.Fatal("No Vault root token provided")
         }
+
+        cl := cleanhttp.DefaultClient()
+        cl.Timeout = time.Second * 60
+
         return &VaultTestServerInfo{
-                Address: "http://127.0.0.1:" + VaultDevPortString,
-                Token:   VaultDevRootToken,
+                pathPrefix: "/v1/secret/data",
+                address:    "http://127.0.0.1:" + VaultDevPortString,
+                token:      VaultDevRootToken,
+                client:     cl,
         }
+}
+
+func (c *TestClient) Write(t *testing.T, path string, data map[string]interface{}) *http.Response {
+        var secret map[string]interface{}
+
+        path = strings.TrimPrefix(path, "/")
+        
+        if hasData(secret) {
+                secret = data
+        } else {
+                secret = map[string]interface{}{
+                        "data": data,
+                }
+        }
+
+        b, err := json.Marshal(secret)
+        if err != nil {
+                t.Fatalf("error marshaling secret: %v", err)
+        }
+
+        req, err := http.NewRequest("PUT", fmt.Sprintf("%s%s/%s", c.address, c.pathPrefix, path), bytes.NewBuffer(b))
+        req.Header.Set("X-Vault-Token", c.token)
+        req.Header.Set("Content-Type", "application/json")
+
+        resp, err := c.client.Do(req)
+        if err != nil {
+                t.Fatalf("error making HTTP request: %v", err)
+        }
+        return resp
+}
+
+func (c *TestClient) Read(t *testing.T, path string) *http.Response {
+        req, err := http.NewRequest("GET", fmt.Sprintf("%s%s/%s", c.address, c.pathPrefix, path), nil)
+        req.Header.Set("X-Vault-Token", c.token)
+        resp, err := c.client.Do(req)
+        if err != nil {
+                t.Fatalf("error making HTTP request: %v", err)
+        }
+        return resp
+}
+
+func hasData(data map[string]interface{}) bool {
+        for k, v := range data {
+                if k == "data" {
+                        return true
+                }
+        }
+        return false
 }
