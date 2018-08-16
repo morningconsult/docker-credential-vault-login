eac331ed71da217cf495cf0f58eac96a916eba50
Dilan Bellinghoven <ddbellinghoven@gmail.com>
2018-08-16 15:52:53 -0400

    main.go: Added version flag

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+<a name="unreleased"></a>
+## [Unreleased]
+
+
+<a name="1.0.0"></a>
+## 1.0.0 - 2018-08-16
+### Aws
+- Added testing
+
+### README
+- Added testing instructions
+
+### Vendor
+- Updated deps
+- Pruned vendors
+- Updated strutil
+- trying to update deps
+
+
+[Unreleased]: https://gitlab.morningconsult.com/mci/docker-credential-vault-login/compare/1.0.0...HEAD
--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+        "fmt"
+        "flag"
         "log"
+        "os"
 
         "github.com/docker/docker-credential-helpers/credentials"
         "gitlab.morningconsult.com/mci/docker-credential-vault-login/vault"
         "gitlab.morningconsult.com/mci/docker-credential-vault-login/vault/helper"
         "gitlab.morningconsult.com/mci/docker-credential-vault-login/vault/config"
+        "gitlab.morningconsult.com/mci/docker-credential-vault-login/vault/version"
 )
 
+const banner = `docker-credential-vault-login
+Version:    %s
+Git commit: %s
+`
+
 func main() {
+        var versionFlag bool
+	flag.BoolVar(&versionFlag, "v", false, "print version and exit")
+	flag.Parse()
+
+	// Exit safely when version is used
+	if versionFlag {
+		fmt.Printf(banner, version.Version, version.GitCommitSHA)
+		os.Exit(0)
+        }
+
         cfg, err := config.GetCredHelperConfig()
         if err != nil {
                 log.Fatalf("Error parsing configuration file: %+v", err)
