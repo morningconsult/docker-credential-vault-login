6dfa4c12172d6d89935136dd0a94462f48c5f11d
Dilan Bellinghoven <ddbellinghoven@gmail.com>
2018-09-11 16:50:41 -0400

    chore: Removed AWS client mocks

--- vault-login/aws/generate_mocks.go
+++
@@ -1,3 +0,0 @@
-package aws
-
-//go:generate mockgen.sh gitlab.morningconsult.com/mci/docker-credential-vault-login/vault-login/aws Client mocks/aws_mocks.go
--- vault-login/vault/factory_test.go
+++ vault-login/vault/factory_test.go
@@ -4,21 +4,17 @@ package vault
 //         "testing"
 
 //         "github.com/aws/aws-sdk-go/awstesting"
-//         gomock "github.com/golang/mock/gomock"
+
 //         test "gitlab.morningconsult.com/mci/docker-credential-vault-login/vault-login/testing"
-//         mock_aws "gitlab.morningconsult.com/mci/docker-credential-vault-login/vault-login/aws/mocks"
 // )
 
-// 
-// cluster := test.StartTestCluster(t)
-// defer cluster.Cleanup()
+// oldEnv := awstesting.StashEnv()
+// defer awstesting.PopEnv(oldEnv)
+// test.SetTestAWSEnvVars()
+
+// server := makeMockVaultServer()
+// go server.Listenandserve()
+
 // client := test.NewPreConfiguredVaultClient(t, cluster)
 // THE TEST:
 // c.getAndSetToken(client, MOCK)
-// func TestTest(t *testing.T) {
-//         ctrl := gomock.NewController(t)
-//         defer ctrl.Finish()
-
-//         awsClient := mock_aws.NewMockClient(ctrl)
-//         awsClient.EXPECT().GetIAMAuthElements(gomock.Any()).Return()
-// }
