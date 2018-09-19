c6de51b570ce3ec4e3e57fe6c59cd81e3af88f56
Dilan Bellinghoven <ddbellinghoven@gmail.com>
2018-09-19 08:56:34 -0400

    scripts/update-deps.sh: Only fetch external packages

--- scripts/update-deps.sh
+++ scripts/update-deps.sh
@@ -29,6 +29,6 @@ govendor init
 
 ## Fetch dependencies
 echo "Fetching dependencies. This will take some time..."
-govendor fetch +missing
+govendor add +external
 
 printf "Done; to commit, run: \n\n    $ cd ${GOPATH}/src/${ORG}/${TOOL}\n\n"
